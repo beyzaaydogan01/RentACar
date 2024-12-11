579defa0c1e2deb96da0e756004f556d81222136
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2024-12-11 21:35:15 +0300

    mapping added

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -15,6 +15,7 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddMediatR(configuration =>
         {
             configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
